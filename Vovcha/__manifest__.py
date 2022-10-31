6fcf74e7343e7f829c916355d2de939cd3378a3d
kofla43 <Volodiay@mail.ru>
2022-10-31 14:50:27 +0400

    31.10.22_2

--- Vovcha/__manifest__.py
+++ Vovcha/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Sale_order_line_number',
+    'name': 'Vovcha',
     'summary': 'Line numbers in Sales Order lines',
     'version': '14.0.0.0.1',
     'category': 'Sales',
-    'author': 'Vladimir',
     'application': False,
     'installable': True,
     'depends': [
-        'sale',
+        "sale",
         ],
     'data': [
     ],
