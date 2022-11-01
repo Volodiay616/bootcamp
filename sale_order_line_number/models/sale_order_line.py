e75b9acce7c328a71df07756ab2f102da641e33c
kofla43 <Volodiay@mail.ru>
2022-11-01 13:47:28 +0400

    compute line number store - true

--- sale_order_line_number/models/sale_order_line.py
+++ sale_order_line_number/models/sale_order_line.py
@@ -1,12 +1,18 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class SaleOrderLine (models.Model):
     _inherit = 'sale.order.line'
     
-    line_number = fields.Integer(compute="_compute_line_number")
+    line_number = fields.Integer(
+        compute = "_compute_line_number",
+        store = True
+        )
     
+    @api.depends("order_id.order_line", "sequence")
     def _compute_line_number(self):
-        n = 1
-        for line in self:
-            line.line_number = n
-            n += 1 
+        order_ids = self.mapped("order_id")
+        for order in order_ids:
+            n = 1
+            for line in order.order_line.sorted(lambda l: l.sequence):
+                line.line_number = n
+                n += 1
